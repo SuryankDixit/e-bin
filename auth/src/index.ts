dc188d75cc19772151964003f6229bfe686ce3ad
SuryankDixit <suryankdixit@gmail.com>
2021-10-31 17:00:48 +0530

    auth server

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,10 +3,17 @@ import  express  from "express";
 const app = express();
 app.use(express.json());
 
-app.get('/api/users/currentuser',(req,res)=>{
-    res.send("Hi there");
-});
+import {currentUserRouter} from './routes/current-user';
+import {signinRouter} from './routes/signin';
+import { signoutRouter } from './routes/signin';
+import { signupRouter } from './routes/signin';
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
 
 app.listen(3000,()=>{
     console.log("Listening on port 3000 !!!!");
 });
+
