56758ca809c3a0e51bc9e15d71dc006d21ee4cc7
SuryankDixit <suryankdixit@gmail.com>
2021-11-01 23:52:40 +0530

    Added users Model and save to database

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -4,35 +4,45 @@ Middleware is checking the validity of email and password
 Body parameter checks the body of the incoming request
 */
 
-import express ,{Request,Response} from 'express';
-import {body, validationResult} from 'express-validator'; 
-import { RequestValidationError } from '../errors/request-validation-error'; 
-import { DatabaseConnectionError } from '../errors/database-connection-error';   
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { User } from '../models/user';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
+router.post(
+    '/api/users/signup',
+    [
+        body('email')
+            .isEmail()
+            .withMessage('Email must be valid'),
+        body('password')
+            .trim()
+            .isLength({ min: 4, max: 20 })
+            .withMessage('Password must be between 4 and 20 characters')
+    ],
+    async (req: Request, res: Response) => {
+        const errors = validationResult(req);
 
-router.post('/api/users/signup', [
+        if (!errors.isEmpty()) {
+            throw new RequestValidationError(errors.array());
+        }
 
-    body('email')
-        .isEmail()
-        .withMessage('Enter valid email'),
-    body('password')
-        .trim()
-        .isLength({min:4,max:20})
-        .withMessage('Password must be betwwen 4 and 20 characters')
-        
-] ,
-async (req:Request, res:Response) => {
+        const { email, password } = req.body;
 
-    const errors = validationResult(req);
+        const existingUser = await User.findOne({ email });
 
-    if(!errors.isEmpty()){
-        throw new RequestValidationError(errors.array());
-    } 
-    console.log('Creating a user..');
-    throw new DatabaseConnectionError();
-    res.send({});
-});
+        if (existingUser) {
+            throw new BadRequestError('Email in use');
+        }
+
+        const user = User.build({ email, password });
+        await user.save();
+
+        res.status(201).send(user);
+    }
+);
 
 export { router as signupRouter };
