f2a99c7e640ba0a951862fabcb0dc64c82ac5406
SuryankDixit <suryankdixit@gmail.com>
2021-11-01 14:16:40 +0530

    Signup route

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -5,7 +5,9 @@ Body parameter checks the body of the incoming request
 */
 
 import express ,{Request,Response} from 'express';
-import {body, validationResult} from 'express-validator';     
+import {body, validationResult} from 'express-validator'; 
+import { RequestValidationError } from '../errors/request-validation-error'; 
+import { DatabaseConnectionError } from '../errors/database-connection-error';   
 
 const router = express.Router();
 
@@ -21,16 +23,15 @@ router.post('/api/users/signup', [
         .withMessage('Password must be betwwen 4 and 20 characters')
         
 ] ,
-(req:Request, res:Response) => {
+async (req:Request, res:Response) => {
 
     const errors = validationResult(req);
 
     if(!errors.isEmpty()){
-        // error object is converted into array of errors.
-        return res.status(400).send(errors.array());
-    }
-    const { email, password} = req.body;
+        throw new RequestValidationError(errors.array());
+    } 
     console.log('Creating a user..');
+    throw new DatabaseConnectionError();
     res.send({});
 });
 
