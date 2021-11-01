ec99047fb79598ece641a809d0b01de53a2c6fb9
SuryankDixit <suryankdixit@gmail.com>
2021-11-01 23:51:55 +0530

    password hashing on save

---
+++ auth/src/services/password.ts
@@ -0,0 +1,24 @@
+import { scrypt, randomBytes } from 'crypto'; 
+import { promisify } from 'util';
+
+/*
+    scrypt is caalback based implementation.
+    so we are converting this callback implementation into promise based implementation
+*/
+const scryptAsync = promisify(scrypt);
+
+export class Password {
+    static async toHash(password: string) {
+        const salt = randomBytes(8).toString('hex');
+        const buf = (await scryptAsync(password, salt, 64)) as Buffer;
+
+        return `${buf.toString('hex')}.${salt}`;
+    }
+
+    static async compare(storedPassword: string, suppliedPassword: string) {
+        const [hashedPassword, salt] = storedPassword.split('.');
+        const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+
+        return buf.toString('hex') === hashedPassword;
+    }
+}
