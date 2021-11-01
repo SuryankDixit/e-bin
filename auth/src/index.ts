e55ee252f4ac6648e3bf6556f3dc271e4d933b44
SuryankDixit <suryankdixit@gmail.com>
2021-11-01 14:15:33 +0530

    added custom error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import  express  from "express";
+import 'express-async-errors'
 
 const app = express();
 app.use(express.json());
@@ -7,11 +8,19 @@ import {currentUserRouter} from './routes/current-user';
 import {signinRouter} from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
+
+app.all('*', async(req,res) => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 app.listen(3000,()=>{
     console.log("Listening on port 3000 !!!!");
