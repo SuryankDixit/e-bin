1a595caae1317b9f9d6fb761d92d8add0a2ec24a
SuryankDixit <suryankdixit@gmail.com>
2021-11-01 20:43:35 +0530

    Added mongodb connection

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import  express  from "express";
-import 'express-async-errors'
+import 'express-async-errors';
+import  mongoose  from "mongoose";
 
 const app = express();
 app.use(express.json());
@@ -22,7 +23,27 @@ app.all('*', async(req,res) => {
 
 app.use(errorHandler);
 
+
+const start = async()=>{
+    /*
+        await mongoose.connect('mongodb://localhost');
+        Here we are trying to connect to mongo db which is a different pod
+
+        auth-mongo-srv is the domain name of the pod, see in yaml file.
+        mention port number
+        give db name 
+    */
+   try{
+       await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+       console.log('Connected to Mongo Db');
+   }catch(err){
+       console.error(err);
+   }
+}
+
+
 app.listen(3000,()=>{
     console.log("Listening on port 3000 !!!!");
 });
 
+start();
