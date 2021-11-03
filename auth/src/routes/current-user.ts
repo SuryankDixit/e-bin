08e9ed8c8cdcca4ff07633955ea771490c8523cf
SuryankDixit <suryankdixit@gmail.com>
2021-11-03 12:47:48 +0530

    Current user test

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,19 @@
+
+/*
+    Whether or not the user is logged in ?
+    does this user have a req.session.jwt set?
+    If it is not set, or if the JWT is invalid, return early.
+    if yes, and jwt is valid, send back the info stored inside the JWT(the payload)
+*/
+
 import express from 'express';
+import { currentUser } from '../middlewares/current-user';
+// import { requireAuth } from '../middlewares/require-auth';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-    res.send("Hi there");
+router.get('/api/users/currentuser', currentUser, (req, res) => {
+    res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
