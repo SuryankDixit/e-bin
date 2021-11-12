959147612b5ea6864665549e4ddb3e1a24309341
SuryankDixit <suryankdixit@gmail.com>
2021-11-12 18:54:05 +0530

    Added Route Handlers

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,11 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@sdebin/common';
+import { errorHandler, NotFoundError,currentUser } from '@sdebin/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 /*
@@ -19,7 +23,11 @@ app.use(
     })
 );
 
-
+app.use(currentUser);
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('*', async (req, res) => {
     throw new NotFoundError();
