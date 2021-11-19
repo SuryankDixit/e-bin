17b2e37a4992ba2ac132132465d3867cdf6fa4d4
SuryankDixit <suryankdixit@gmail.com>
2021-11-19 16:10:03 +0530

    server setup

---
+++ orders/src/app.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError,currentUser } from '@sdebin/common';
+
+import { deleteOrderRouter } from './routes/delete';
+import { indexOrderRouter } from './routes/index';
+import { newOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
+const app = express();
+/*
+we are just adding in this little step right here to make sure that Express is aware
+that it's behind a proxy of Ingress and Genex and to make sure that it should still trust traffic as
+being secure, even though it's coming from that proxy.
+*/
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: process.env.NODE_ENV !== 'test'
+    })
+);
+
+app.use(currentUser);
+app.use(deleteOrderRouter);
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+
+app.all('*', async (req, res) => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
