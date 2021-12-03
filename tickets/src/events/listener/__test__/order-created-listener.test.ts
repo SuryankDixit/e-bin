b4f68d2f37fec975ad34cffdf22c1820b50fd5f3
SuryankDixit <suryankdixit@gmail.com>
2021-12-03 12:21:00 +0530

    Added Test

--- tickets/src/events/listener/__test__/order-created-listener.test.ts
+++ tickets/src/events/listener/__test__/order-created-listener.test.ts
@@ -1,6 +1,6 @@
 import { Message } from 'node-nats-streaming';
 import mongoose from 'mongoose';
-import { OrderCreatedEvent, OrderStatus } from '@cygnetops/common';
+import { OrderCreatedEvent, OrderStatus } from '@sdebin/common';
 import { OrderCreatedListener } from '../order-created-listener';
 import { natsWrapper } from '../../../nats-wrapper';
 import { Ticket } from '../../../models/ticket';
@@ -19,7 +19,7 @@ const setup = async () => {
 
   // Create the fake data event
   const data: OrderCreatedEvent['data'] = {
-    id: mongoose.Types.ObjectId().toHexString(),
+    id: new mongoose.Types.ObjectId().toHexString(),
     version: 0,
     status: OrderStatus.Created,
     userId: 'alskdfj',
@@ -53,4 +53,19 @@ it('acks the message', async () => {
   await listener.onMessage(data, msg);
 
   expect(msg.ack).toHaveBeenCalled();
+});
+
+
+it('publishes a ticket updated event', async () => {
+  const { listener, ticket, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+
+  const ticketUpdatedData = JSON.parse(
+    (natsWrapper.client.publish as jest.Mock).mock.calls[0][1]
+  );
+
+  expect(data.id).toEqual(ticketUpdatedData.orderId);
 });
